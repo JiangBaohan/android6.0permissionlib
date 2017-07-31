eb81083302f38ebd26ee5994a593b4649c5e9c45
JiangBaohan <498805449@qq.com>
2017-07-31 17:32:22 +0800

    Android6.0运行时权限处理

---
+++ android6.0permission封装/src/main/java/com/sn/android60permission/Constants.java
@@ -0,0 +1,11 @@
+package com.sn.android60permission;
+
+
+
+public class Constants {
+    /**
+     * 权限相关的常量
+     */
+    public static final int CALL_PHONE = 67;//打电话
+    public static final int WRITE_EXTERNAL_STORAGE = 458;//SD卡写入
+}
